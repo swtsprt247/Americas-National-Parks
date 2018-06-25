b717afd6cd355fcdbcf089e2f0a09cd77bf59bcb
Brenda Wilkins <32784482+swtsprt247@users.noreply.github.com>
2018-06-24 20:09:36 -0500

    Add files via upload

---
+++ articles.py
@@ -0,0 +1,26 @@
+def NewsReleases ():
+    newsReleases = [
+        {
+            'id': 1,
+            'parkCode': 'Park 1',
+            'releaseDate': '03-05-2018',
+            'title': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim',
+            'url': 'url'
+        },
+        {
+            'id': 2,
+            'parkCode': 'Park 2',
+            'releaseDate': '03-06-2018',
+            'title': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim',
+            'url': 'url'
+        },
+        {
+            'id': 3,
+            'parkCode': 'Park 3',
+            'releaseDate': '03-07-2018',
+            'title': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim',
+            'url': 'https://www.nps.gov/zion/learn/news/zion-national-park-and-its-official-nonprofit-partner-zion-national-park-forever-project-announce-the-10th-annual-plein-air-art-invitational-dates-and-featured-artists.htm'
+        }
+    ]
+    return newsReleases
+    
